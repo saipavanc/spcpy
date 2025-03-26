f1b04440d6c6054c389c20c67a5622f9b782c369
saipavanc <79005467+saipavanc@users.noreply.github.com>
2025-03-26 11:50:09 -0500

    Add progressbar

--- spcpy/misc_utils.py
+++ spcpy/misc_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Tuple
-
+import sys
+import time
 
 def despike(
     phi_vals, freqs, slope_threshold=0.25e-5, width=10e-2
@@ -42,3 +43,28 @@ def despike(
     idxs_remove = np.sort(np.unique(idxs_remove))
     idxs_filtered = [idx for idx in range(len(phi_vals)) if idx not in idxs_remove]
     return phi_vals[idxs_filtered], freqs[idxs_filtered]
+
+class ProgressBar:
+    def __init__(self, num_iterations, fill_char='-', width=50):
+        self.num_iterations = num_iterations
+        self.fill_char = fill_char
+        self.empty_char = ' '
+        self.width = width
+        self.prog_bar = self.progress_bar_str(0) # This will show the progress bar as empty
+        self.time_after_last_iter = time.time()
+        self.time_for_last_iter = None
+        self.update(0)
+        
+    def progress_bar_str(self, iteration):
+        if iteration > self.num_iterations:
+            raise ValueError('iteration is larger than num_iterations')
+        return "[" + self.fill_char * int(self.width * iteration / self.num_iterations)  + self.empty_char * int(self.width * (self.num_iterations - iteration) / self.num_iterations) + "]"
+        
+    def update(self, iteration):
+        self.prog_bar = self.progress_bar_str(iteration).ljust(self.width)
+        if iteration > 0:
+            current_time = time.time()
+            self.time_for_last_iter = round(current_time - self.time_after_last_iter, 3)
+            self.time_after_last_iter = time.time()
+        sys.stdout.write('\r{0} {1}% {2}'.format(self.prog_bar, int(100 * iteration / self.num_iterations), "Last iter: " + str(self.time_for_last_iter) + "s" if iteration > 0 else ""))
+        sys.stdout.flush()
