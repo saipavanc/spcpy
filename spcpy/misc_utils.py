39dcd5e0a06946b16c6f65dbee1aabcff51c3d2f
saipavanc <79005467+saipavanc@users.noreply.github.com>
2025-04-15 15:57:30 -0500

    Move methods to plotting_utils

--- spcpy/misc_utils.py
+++ spcpy/misc_utils.py
@@ -3,47 +3,6 @@ from typing import Tuple
 import sys
 import time
 
-def despike(
-    phi_vals, freqs, slope_threshold=0.25e-5, width=10e-2
-) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Remove spikes from a curve by filtering out points, and their neighbouring points,
-    with a slope above slope_threshold.
-
-    Args:
-        phi_vals (array): pts on x-axis
-        freqs (array): pts on y-axis
-        slope_threshold (float, optional): The threshold of slope over which peaks will be removed.
-                                            Defaults to 0.25e-5.
-        width (float, optional): The width on x-axis over which the neighbouring pts will be removed.
-                                    Defaults to 10e-2.
-
-    Returns:
-        Tuple[np.ndarray, np.ndarray]: phi_vals and freqs after removing spikes.
-    """
-    slopes = np.abs(np.gradient(freqs))
-    idxs_slopes = [idx for idx in range(len(slopes)) if slopes[idx] > slope_threshold]
-    # width of peaks or dips in units of the x-axis
-    dx = phi_vals[1] - phi_vals[0]
-    number_of_pts_around_peaks = int(width / dx)
-    idxs_remove = idxs_slopes.copy()
-
-    for idx in idxs_slopes:
-        additional_pts = np.array(
-            [
-                idx + i
-                for i in range(-number_of_pts_around_peaks, number_of_pts_around_peaks)
-            ]
-        )
-        additional_pts = additional_pts[
-            (additional_pts >= 0) & (additional_pts < len(phi_vals))
-        ]
-        idxs_remove.extend(additional_pts)
-
-    idxs_remove = np.sort(np.unique(idxs_remove))
-    idxs_filtered = [idx for idx in range(len(phi_vals)) if idx not in idxs_remove]
-    return phi_vals[idxs_filtered], freqs[idxs_filtered]
-
 class ProgressBar:
     def __init__(self, num_iterations, fill_char='-', width=50):
         self.num_iterations = num_iterations
--- spcpy/plotting_utils.py
+++ spcpy/plotting_utils.py
@@ -9,6 +9,7 @@ from matplotlib.image import AxesImage
 from matplotlib.lines import Line2D
 from matplotlib.patches import Rectangle
 from matplotlib.text import Text
+from typing import Dict, Optional, Tuple, Union, List
 
 
 class Plot:
@@ -52,3 +53,88 @@ class Plot:
             num_pts (int): number of points to be clicked on the plot.
         """
         self.fig.ginput(num_pts, timeout=0, show_clicks=True)
+
+
+def despike(
+    phi_vals, freqs, slope_threshold=0.25e-5, width=10e-2
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Remove spikes from a curve by filtering out points, and their neighbouring points,
+    with a slope above slope_threshold.
+
+    Args:
+        phi_vals (array): pts on x-axis
+        freqs (array): pts on y-axis
+        slope_threshold (float, optional): The threshold of slope over which peaks will be removed.
+                                            Defaults to 0.25e-5.
+        width (float, optional): The width on x-axis over which the neighbouring pts will be removed.
+                                    Defaults to 10e-2.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: phi_vals and freqs after removing spikes.
+    """
+    slopes = np.abs(np.gradient(freqs))
+    idxs_slopes = [idx for idx in range(len(slopes)) if slopes[idx] > slope_threshold]
+    # width of peaks or dips in units of the x-axis
+    dx = phi_vals[1] - phi_vals[0]
+    number_of_pts_around_peaks = int(width / dx)
+    idxs_remove = idxs_slopes.copy()
+
+    for idx in idxs_slopes:
+        additional_pts = np.array(
+            [
+                idx + i
+                for i in range(-number_of_pts_around_peaks, number_of_pts_around_peaks)
+            ]
+        )
+        additional_pts = additional_pts[
+            (additional_pts >= 0) & (additional_pts < len(phi_vals))
+        ]
+        idxs_remove.extend(additional_pts)
+
+    idxs_remove = np.sort(np.unique(idxs_remove))
+    idxs_filtered = [idx for idx in range(len(phi_vals)) if idx not in idxs_remove]
+    return phi_vals[idxs_filtered], freqs[idxs_filtered]
+
+
+def moving_average(data, window=2):
+    """Makes a moving average of the data.
+
+    Parameters
+    ----------
+    data
+        Array like object
+    window, optional
+        The window size for the moving average. The default is 2.
+    """
+    averaged_data = data.copy()
+    for idx, y in enumerate(data):
+        subset = data[(idx-window//2) if idx >= window//2 else 0:idx+window//2+1]
+        averaged_data[idx] = sum(subset)/len(subset)
+    return averaged_data
+
+def filter(x_vals, y_vals, y_range: tuple):
+    """
+    Used to filter out all the points which are not in the y_range.
+
+    Parameters
+    ----------
+    x_vals
+        array like object
+    y_vals
+        array like object
+    y_range
+        tuple showing the range of y values to be kept.
+
+    Returns
+    -------
+        All the data points which are in the y_range.
+    """
+    x_vals_copy = np.array(x_vals.copy())
+    y_vals_copy = np.array(y_vals.copy())
+    filtered_out_idxs = []
+    for idx, y_val in enumerate(y_vals):
+        if not y_range[0] <= y_val <= y_range[1]:
+            filtered_out_idxs.append(idx)
+    result_idxs = [idx for idx in range(len(x_vals)) if idx not in filtered_out_idxs]
+    return x_vals_copy[result_idxs], y_vals_copy[result_idxs]
